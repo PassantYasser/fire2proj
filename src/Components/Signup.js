67f4bf70bb2d30141b295e854701009c88b5fab2
PassantYasser <passantabutalib@gmail.com>
2023-11-01 13:30:17 +0200

    upadate

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,74 +1,69 @@
-import { React, useRef, useState } from "react";
-import { useAuth } from "../context/AuthContext";
-import { Form, Button, Card, Alert } from "react-bootstrap";
-
-import { Link, useNavigate } from "react-router-dom";
+import React, { useState,useRef } from 'react';
+import {Card ,Form,Button,Alert} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
+import {useAuth} from '../Context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 export default function Signup() {
+  const {signup}= useAuth();
+  const[Error , setError]=useState("")
+  const[Loading , setLoading]=useState(false);
   const emailRef = useRef();
   const passwordRef = useRef();
-  const passwordConfirmRef = useRef();
-  const { signup } = useAuth();
-  const [error, setError] = useState("");
-  const [loading, setLoading] = useState(false);
+  const passwordconfirmationRef = useRef();
   const navigate = useNavigate();
 
-  async function handleSubmit(e) {
-    e.preventDefault();
-    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-      return setError("Passwords do not match");
-    }
-    try {
+    const handleSubmit = async(e)=>{
+      e.preventDefault();
+      if(passwordRef.current.value !== passwordconfirmationRef.current.value){
+        return setError("password do not match")
+      }
+    try{
       setError("");
       setLoading(true);
-      await signup(emailRef.current.value, passwordRef.current.value);
-      navigate("/");
-    } catch {
-      setError("Failed to create an account");
-    }
-    setLoading(false);
-  }
+      await signup(emailRef.current.value ,passwordRef.current.value )
+      navigate('/');
+    }catch{
+      setError('faild to create an account');
 
-  return (
-    <>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Sign Up</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <Form onSubmit={handleSubmit}>
-            <Form.Group>
-              <Form.Label htmlFor="email">Email</Form.Label>
-              <Form.Control id="email" type="email" ref={emailRef} required />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label htmlFor="password">Password</Form.Label>
-              <Form.Control
-                id="password"
-                type="password"
-                ref={passwordRef}
-                required
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label htmlFor="password-confirm">
-                Password Confirmation
-              </Form.Label>
-              <Form.Control
-                id="password-confirm"
-                type="password"
-                ref={passwordConfirmRef}
-                required
-              />
-            </Form.Group>
-            <Button disabled={loading} className="w-100 mt-3" type="submit">
-              Sign Up
-            </Button>
-          </Form>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        Already have an account? <Link to="/login">Log In</Link>
-      </div>
-    </>
-  );
+    }
+    setLoading(false)
+    
+  }
+  return (<>
+    <Card className='bg-dark'>
+      <Card.Body>
+          <h2 className='text-center mb-4 text-light'>Signup</h2>
+          {Error && <Alert variant="warning">{Error}</Alert>}
+        <Form onSubmit={handleSubmit}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label className='text-light'>Email</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" id="Email" ref={emailRef}/>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label className='text-light'>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" id="Password"ref={passwordRef} />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label className='text-light'>ConfirmPassword</Form.Label>
+        <Form.Control type="password" placeholder="ConfirmPassword" id="ConfirmPassword" ref={passwordconfirmationRef} />
+      </Form.Group>
+      
+      <Button variant="primary" type="submit" className='w-100 mt-3'disabled={Loading} >
+        Signup
+      </Button>
+    </Form>
+      </Card.Body>
+
+    </Card>
+  <div className='w-100 text-center mt-2'>
+    Already have an account <Link to="/Login">Login</Link>
+  </div>
+  
+  
+  
+  
+  </>
+  )
 }
+
